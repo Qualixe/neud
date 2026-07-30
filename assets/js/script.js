fb0d06d7038e10f2bbe91701c96ed3badd2a9e50
Limon ALi <limonali105@gmail.com>
2026-07-30 10:25:04 +0600

    footer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,16 @@
 $(document).ready(function () {
   "use strict";
   // navbar js start ---
-//   $(window).on("load scroll", function () {
-//     var scrolling = $(this).scrollTop();
-//     if (scrolling > 50) {
-//       $(".navbar").addClass("nav-fixed");
-//       $(".home-nav-active").removeClass("home-nav");
-//     } else {
-//       $(".navbar").removeClass("nav-fixed");
-//       $(".home-nav-active").addClass("home-nav");
-//     }
-//   });
-
-  let lastScroll = 0;
-
-  window.addEventListener("load", handleScroll);
-  window.addEventListener("scroll", handleScroll);
-
-  function handleScroll() {
-    const scrolling = window.scrollY;
-    const navbar = document.querySelector(".navbar");
-    const home_nav_active = document.querySelector(".home-nav-active");
-
-    if (scrolling > 1) {
-      navbar.classList.add("nav-fixed");
-      home_nav_active.classList.remove("home-nav");
-    } else {
-      navbar.classList.remove("nav-fixed", "nav-hidden");
-      home_nav_active.classList.add("home-nav");
-    }
-
-    if (scrolling > lastScroll && scrolling > 10) {
-      // scrolling down — hide header
-      navbar.classList.add("nav-hidden");
-    } else if (scrolling < lastScroll) {
-      // scrolling up — show header
-      navbar.classList.remove("nav-hidden");
-    }
-
-    lastScroll = scrolling;
-  }
+  //   $(window).on("load scroll", function () {
+  //     var scrolling = $(this).scrollTop();
+  //     if (scrolling > 50) {
+  //       $(".navbar").addClass("nav-fixed");
+  //       $(".home-nav-active").removeClass("home-nav");
+  //     } else {
+  //       $(".navbar").removeClass("nav-fixed");
+  //       $(".home-nav-active").addClass("home-nav");
+  //     }
+  //   });
 
   // cart-sidebar js start---
   $(".side-cart-btn").click(function (event) {
@@ -72,3 +43,32 @@ $(document).ready(function () {
   });
   // sticky-add-to-cart-section js end--
 });
+
+let lastScroll = 0;
+
+window.addEventListener("load", handleScroll);
+window.addEventListener("scroll", handleScroll);
+
+function handleScroll() {
+  const scrolling = window.scrollY;
+  const navbar = document.querySelector(".navbar");
+  const home_nav_active = document.querySelector(".home-nav-active");
+
+  if (scrolling > 1) {
+    navbar.classList.add("nav-fixed");
+    home_nav_active.classList.remove("home-nav");
+  } else {
+    navbar.classList.remove("nav-fixed", "nav-hidden");
+    home_nav_active.classList.add("home-nav");
+  }
+
+  if (scrolling > lastScroll && scrolling > 10) {
+    // scrolling down — hide header
+    navbar.classList.add("nav-hidden");
+  } else if (scrolling < lastScroll) {
+    // scrolling up — show header
+    navbar.classList.remove("nav-hidden");
+  }
+
+  lastScroll = scrolling;
+}
