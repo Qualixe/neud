13db9d89a4b1df4aa5bf89ab15cefc473e98e7c6
Limon ALi <limonali105@gmail.com>
2026-08-01 11:10:01 +0600

    removed sticky header, added grid view in all requared section

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,25 +55,11 @@ function handleScroll() {
   const navbar = document.querySelector(".navbar");
   const home_nav_active = document.querySelector(".home-nav-active");
 
-  if (scrolling > 1) {
-    navbar.classList.add("nav-fixed");
-  } else {
-    navbar.classList.remove("nav-fixed", "nav-hidden");
-  }
-
-  if (scrolling > lastScroll && scrolling > 10) {
-    // scrolling down — hide header
-    navbar.classList.add("nav-hidden");
-  } else if (scrolling < lastScroll) {
-    // scrolling up — show header
-    navbar.classList.remove("nav-hidden");
-  }
-
   lastScroll = scrolling;
 
-  if ((scrolling > 1) & navbar.classList.contains("home-nav-active")) {
+  if ((scrolling > 100) & navbar.classList.contains("home-nav-active")) {
     home_nav_active.classList.remove("home-nav");
-  } else if ((scrolling < 1) & navbar.classList.contains("home-nav-active")) {
+  } else if ((scrolling < 100) & navbar.classList.contains("home-nav-active")) {
     home_nav_active.classList.add("home-nav");
   }
 }
